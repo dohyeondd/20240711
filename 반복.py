9bd27ecedf80a7ac5ff8234c0f7b2184130944a9
dohyeondd <parkdohyeon1235@gmail.com>
2024-07-11 17:23:25 +0900

    20240711
    dd

---
+++ 반복.py
@@ -0,0 +1,46 @@
+
+# # for 변수 in 변수 할당 가능한 자료(집합 개념을 가지고 있는 것) :
+# # 변수에 자료가 할당되는 동안 수행할 문장1
+# # 변수에 자료가 할당되는 동안 수행할 문자2
+# # 변수는 원하는 값으로 지정가능
+# # 들여쓰기 취소하고 하면 반복되지 않는다.
+
+my list = {100,200,400,800,1000,1300}
+result = 0
+for i in my list :
+    result = result+ i
+  avg = result / len(my_list)
+    print(int(avg))
+
+    # print(i)
+
+
+
+
+# # 데이터안에 뭐가 들어있는지 화면에 알아서 출력 해줘
+# fruits =["사과","바나나","딸기"]
+# for var in fruits :
+#     print("이는 첫번째 코드블록 라인입니다.", var)
+#     print("이는 첫번째 코드블록 라인입니다.", var)
+# print("이는 for 반복문의  코드블록 밖입니다.")
+#
+# #1 데이터가 주어짐
+# numbers = [100,200,300]
+# #2 반복문으르 사용해서 부가세를 더하는 연산 추가
+# for num in numbers :
+#     print(int(num *1.1))
+
+
+# for 반복문 예제
+# string ="오늘은 더벤요,그래요."
+# for s in string :
+#     print(s)
+
+# numbers= [3,-20, -3, 44]
+# for num in numbers :
+#         if num < 0 :
+#             print(num)
+
+
+while
+
---
+++ 연습.py
@@ -0,0 +1,60 @@
+# # # # for i in range(0, 10, 3) :
+# # # #     print(i)
+# # #
+# # #
+# # # result =[]
+# # # for i in range(0,100) :
+# # #     result.append(i)
+# # #     print(i)
+# # #
+# # # print(result)
+# # #
+# # # # while 조건문1 :
+# # # #     변수에 자료가 할당되는 동안 수행할 코드블록
+# # # #     조건문은 참 거짓을 나타태는 논린 연산 값
+# # # # else 조건문이 거짓을 경우 코드를 반복하지 않음
+# #
+# #
+# # i = 10
+# # while  i <100 :
+# #     i = i+1
+# #     print("i는 지금 값이",i,"이므로, 100보다 작습니다.")
+#
+# i = 0
+# while i <1000 :
+#     i=i+1
+#     print(i)
+
+
+# while문을 사용하여 1부터 100까지중 홀수의 합을 구하시오
+
+#1 1부터 100까지의 수를 반복 출력
+
+#2 반복되는 수들에서 홀수이지를 확인
+
+#3 홀수라면 그 값을 저장
+
+#4 저장된 값들의 합을 구함
+
+#
+# b =[]
+# a = 0
+# while a<100:
+#     a=a+1
+#     if a%2 ==1:
+#         b.append(a)
+#
+# c = sum(b)
+# print(c)
+
+
+import random
+
+
+
+i = 0
+result =[]
+while i<6
+    i =i+1
+result, append =random. randint(1, 45)
+print(result)
